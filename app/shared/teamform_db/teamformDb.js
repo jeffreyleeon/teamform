0154b089958f40a3fe9059a4637d3e4c980fb9da
Lee Chun On <jeffreyleeon@gmail.com>
2016-10-03 00:12:37 +0800

    Move db related code in teamController to teamformDb.js

--- app/shared/teamform_db/teamformDb.js
+++ app/shared/teamform_db/teamformDb.js
@@ -5,7 +5,10 @@ function TeamformDb($firebaseObject, $firebaseArray, modelsFactory) {
 	initalizeFirebase();
   var service = {
     getEvent: getEvent,
+    getEventAdminData: getEventAdminData,
     getAllTeams: getAllTeams,
+    getTeam: getTeam,
+    setTeamData: setTeamData,
     getAllMembers: getAllMembers,
   };
   return service;
@@ -19,9 +22,25 @@ function TeamformDb($firebaseObject, $firebaseArray, modelsFactory) {
   	return event;
   }
 
+  function getEventAdminData(eventName, callback) {
+  	var refPath =  eventName + "/admin";
+  	retrieveOnceFirebase(firebase, refPath, callback);
+  }
+
   function getAllTeams(eventName) {
   	var refPath = eventName + '/team';
     return $firebaseArray(firebase.database().ref(refPath));
+  };
+
+  function getTeam(eventName, teamID, callback) {
+		var refPath = eventName + "/team/" + teamID;
+		retrieveOnceFirebase(firebase, refPath, callback);
+  };
+
+  function setTeamData(eventName, teamID, data, callback) {
+    var refPath = eventName + "/team/" + teamID; 
+    var ref = firebase.database().ref(refPath);
+    ref.set(data, callback);
   };
 
   function getAllMembers(eventName) {
