646972fccf5a944feb2dc6178a1eac2cef85f0f0
christse02 <christse02@gmail.com>
2016-11-02 21:36:01 +0800

    FB login
    fb login enabled in firebase

--- app/config/env.js
+++ app/config/env.js
@@ -1,18 +1,14 @@
 env = {
+/*
   FB_API_KEY: 'AIzaSyDTXQFSuriwxpvJd0mZHElmLmhL8AIYmWE',
   FB_AUTH_DOMAIN: 'teamform-15bcb.firebaseapp.com',
   FB_DATABASE_URL: 'https://teamform-15bcb.firebaseio.com',
   FB_STORAGE_BUCKET: 'teamform-15bcb.appspot.com'
+*/
+  FB_API_KEY: 'AIzaSyDULfYNZxcZgu5OYtI3aOjDuVLU_8ngYvU',
+  FB_AUTH_DOMAIN: 'comp3111project-88170.firebaseapp.com',
+  FB_DATABASE_URL: 'https://comp3111project-88170.firebaseio.com',
+  FB_STORAGE_BUCKET: 'comp3111project-88170.appspot.com'
 }
 
-//<script src="https://www.gstatic.com/firebasejs/3.5.3/firebase.js"></script>
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDULfYNZxcZgu5OYtI3aOjDuVLU_8ngYvU",
-    authDomain: "comp3111project-88170.firebaseapp.com",
-    databaseURL: "https://comp3111project-88170.firebaseio.com",
-    storageBucket: "comp3111project-88170.appspot.com",
-    messagingSenderId: "975041094299"
-  };
   firebase.initializeApp(config);
