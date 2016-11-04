c93e93a54ee4a292d1d03dc9665af38dee6991b8
Lee Chun On <jeffreyleeon@gmail.com>
2016-11-04 11:19:04 +0800

    Remove unused member.html page

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,6 @@ module.exports = function(config) {
 	preprocessors: {	 	
 		 'assets/js/site.js' : ['coverage'],		
 		 'assets/js/team.js' : ['coverage'],
-		 'assets/js/member.js' : ['coverage'],
 		 'config/*.js': ['coverage'],
 		 'components/*/*.js': ['coverage'],
 		 'shared/*/*.js': ['coverage'],
