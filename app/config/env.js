ab35f450bedef84b78174999aac6f341440f87c7
Vivian Rajkumar <paintfrussianblue@gmail.com>
2016-11-13 15:40:18 +0800

    Ignore env.js

--- app/config/env.js
+++
@@ -1,6 +0,0 @@
-env = {
-  FB_API_KEY: 'AIzaSyDTXQFSuriwxpvJd0mZHElmLmhL8AIYmWE',
-  FB_AUTH_DOMAIN: 'teamform-15bcb.firebaseapp.com',
-  FB_DATABASE_URL: 'https://teamform-15bcb.firebaseio.com',
-  FB_STORAGE_BUCKET: 'teamform-15bcb.appspot.com'
-}
